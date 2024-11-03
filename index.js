49a845434f117bf55ef4aaa585acb6f14d16d8ad
Nafis Nihal <nafismihal12@gmail.com>
2024-11-03 16:59:22 +0600

    Refactor static file serving and update Vercel routing configuration

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
